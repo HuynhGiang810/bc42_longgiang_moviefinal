2ab95418345720feed9f706fecb480484dff0c66
Huynh Giang <huynhgiang@tanhuythinh.com>
2023-07-11 09:44:55 +0700

    check

---
+++ src/API/movieAPI.js
@@ -0,0 +1,80 @@
+import axiosmain from "./axiosmain";
+
+
+
+export const apiGetMovies = async () => {
+    const { data } = await axiosmain.get("/QuanLyPhim/LayDanhSachPhim", {
+        params: {
+            maNhom: "GP14",
+        },
+    });
+    return data;
+};
+
+export const apiGetBanners = async () => {
+    const { data } = await axiosmain.get("/QuanLyPhim/LayDanhSachBanner");
+    return data;
+};
+
+export const apiGetMovieDetails = async (movieId) => {
+    const { data } = await axiosmain.get("/QuanLyPhim/LayThongTinPhim", {
+        params: {
+            MaPhim: movieId,
+        },
+    });
+
+    return data;
+};
+
+export const apiCreateMovie = async (movie) => {
+    const formData = new FormData();
+    for (let key in movie) {
+        formData.append(key, movie[key]);
+    }
+    formData.append("maNhom", "GP14");
+
+    await axiosmain.post("/QuanLyPhim/ThemPhimUploadHinh", formData);
+};
+
+
+
+export const apiGetHeThongRap = async () => {
+    const { data } = await axiosmain.get("/QuanLyRap/LayThongTinHeThongRap");
+    return data;
+};
+
+export const apiGetCumRap = async (cinemaID) => {
+    const { data } = await axiosmain.get(
+        "/QuanLyRap/LayThongTinCumRapTheoHeThong",
+        {
+            params: {
+                maHeThongRap: cinemaID,
+            },
+        }
+    );
+    return data;
+};
+
+export const apiGetLichChieuRap = async (cinemaID) => {
+    const { data } = await axiosmain.get(
+        "/QuanLyRap/LayThongTinLichChieuHeThongRap?maNhom=GP14",
+        {
+            params: {
+                maHeThongRap: cinemaID,
+            },
+        }
+    );
+    return data;
+};
+
+export const apiGetLichChieuPhim = async (maPhim) => {
+    const { data } = await axiosmain.get(
+        "/QuanLyRap/LayThongTinLichChieuPhim",
+        {
+            params: {
+                MaPhim: maPhim,
+            },
+        }
+    );
+    return data;
+};
--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Home from './modules/Home/Home';
+import { Route, Router, Routes } from 'react-router-dom';
+import MovieDetails from './modules/MovieDetails/MovieDetails';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Home/>}/>
+        <Route path="/movies/:movieId" element={<MovieDetails/>}/>
+      </Routes>
+    </Router>
   );
 }
 
